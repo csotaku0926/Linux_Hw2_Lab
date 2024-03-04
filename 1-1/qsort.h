ed5acdd8cd80c1d8ff4e1920567de4e42088b1d1
ben900926 <ben900926.cs09@nycu.edu.tw>
2024-03-05 01:19:39 +0800

    Add the missing part in hw2 1-1 part
    The original quick sort has some parts missing. Fill in these values.
    It should now work as a non-recursive sort.
    
    Need to apply linux kernel list functions on it, and make improvments.

---
+++ 1-1/qsort.h
@@ -0,0 +1,27 @@
+#ifndef LAB1_QSORT_H
+#define LAB1_QSORT_H 
+
+/* 
+    The original implementation: https://hackmd.io/@sysprog/linux2024-quiz1
+*/
+
+typedef struct __node {
+    struct __node *left, *right;
+    struct __node *next;
+    long value;
+} node_t;
+
+// add node_t to beginning of list
+void list_add(node_t **list, node_t *node_t);
+
+node_t *list_tail(node_t **left);
+
+int list_length(node_t **left);
+
+node_t *list_construct(node_t *list, int n);
+
+void list_free(node_t **list);
+
+void quick_sort(node_t **list);
+
+#endif
